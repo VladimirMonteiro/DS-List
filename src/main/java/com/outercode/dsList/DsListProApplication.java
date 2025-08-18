6b1c35d6161022a0892511c8b2007304ea5351f3
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-08-18 19:48:22 -0300

    init project

---
+++ src/main/java/com/outercode/dsList/DsListProApplication.java
@@ -0,0 +1,13 @@
+package com.outercode.dsList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outercode/dsList/DsListProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outercode.dsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
