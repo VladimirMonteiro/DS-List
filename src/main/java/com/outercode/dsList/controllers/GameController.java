a0fe0f7b1c4d97b0612361e9a6954a44fbfbdc9a
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-08-20 18:58:31 -0300

    doc: documented controllers

--- src/main/java/com/outercode/dsList/controllers/GameController.java
+++ src/main/java/com/outercode/dsList/controllers/GameController.java
@@ -3,6 +3,9 @@ package com.outercode.dsList.controllers;
 import com.outercode.dsList.dto.GameDTO;
 import com.outercode.dsList.dto.GameMinDTO;
 import com.outercode.dsList.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +18,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/games", produces = "application/json")
+@Tag(name = "Games", description = "Controller for managing games")
 public class GameController {
 
     @Autowired
     private GameService gameService;
 
     @GetMapping
+    @Operation(summary = "Find all games.", description = "This feature returns all games available in the system.")
+    @ApiResponse(responseCode = "200", description = "Returned list of games.")
     public ResponseEntity<List<GameMinDTO>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(gameService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find game by ID.", description = "This feature returns a game by its ID.")
+    @ApiResponse(responseCode = "200", description = "Returned game details by ID.")
     public ResponseEntity<GameDTO> findById(@PathVariable("id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(gameService.findById(id));
     }
--- src/main/java/com/outercode/dsList/controllers/GameListController.java
+++ src/main/java/com/outercode/dsList/controllers/GameListController.java
@@ -4,6 +4,9 @@ import com.outercode.dsList.dto.GameListDTO;
 import com.outercode.dsList.dto.GameMinDTO;
 import com.outercode.dsList.services.GameListService;
 import com.outercode.dsList.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/game-lists", produces = "application/json")
+@Tag(name = "Games list", description = "Controller from manage of games lists")
 public class GameListController {
 
     @Autowired
@@ -25,13 +29,16 @@ public class GameListController {
     private GameService gameService;
 
     @GetMapping
+    @Operation(summary = "Find all list of games.", description = "This feature return all game lists available in the system.")
+    @ApiResponse(responseCode = "200", description = "Returned list of games.")
     public ResponseEntity<List<GameListDTO>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(gameListService.findAll());
     }
 
     @GetMapping("/{listId}/games")
+    @Operation(summary = "Find all games of one list using id.", description = "This feature return all games of an list available in the system.")
+    @ApiResponse(responseCode = "200", description = "Returned games of one list using id.")
     public ResponseEntity<List<GameMinDTO>> findByList(@PathVariable("listId") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(gameService.findByList(id));
     }
-
 }
