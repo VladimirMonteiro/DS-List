cb03dedc5d4580e95be3d638af454c90f908b9cf
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-08-20 14:38:03 -0300

    fix: homolog ok

--- src/main/java/com/outercode/dsList/dto/GameMinDTO.java
+++ src/main/java/com/outercode/dsList/dto/GameMinDTO.java
@@ -10,6 +10,6 @@ public record GameMinDTO(Long id, String title, Integer year, String imgUrl, Str
     }
 
     public GameMinDTO(GameMinProjection projection) {
-        this(projection.getId(), projection.getTitle(), projection.getYear(), projection.getImgUrl(), projection.getShortDescription());
+        this(projection.getId(), projection.getTitle(), projection.getGameYear(), projection.getImgUrl(), projection.getShortDescription());
     }
 }
